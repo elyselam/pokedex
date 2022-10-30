094ebd194ff41095b1efedd5fccda3205bd8a546
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2022-10-29 18:21:16 -0700

    feat: display name and images in grids

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import './styles.css';
 
 const Home = () => {
   const [pokemons, setPokemons] = useState([]);
@@ -7,44 +8,36 @@ const Home = () => {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
     const data = await response.json();
     setPokemons(data.results);
-    console.log(pokemons);
+
   }, []);
-  console.log(pokemons);
+
+
+  const getID = (pokemon) => {
+    let splitURL = pokemon.url.split("/");
+    return splitURL[splitURL.length - 2];
+  };
+
   return (
     <div>
       <button className="ui primary button">Show All</button>
       <div class="container">
         {pokemons.map((pokemon) => (
           <div class="img-container">
-            <h2>Name: {pokemon.name}</h2>
+            <h2>Name: {pokemon.name} </h2>
             <div class="img-item">
-              
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getID(
+                  pokemon
+                )}.png`}
+              />
             </div>
           </div>
         ))}
       </div>
-      )
     </div>
   );
 
-  //   <style>
-  //     .container {
-  //     display: flex;
-  //     flex-wrap: wrap;
-  //     text-align: center;
 
-  // }
-
-  // .img-container {
-  // border: dotted 2px black;
-  // width: 600px;
-  // height: 600px;
-  // min-width: auto;
-  // min-height: auto;
-  // overflow: hidden;
-  // }
-  //   </style>
-};;
-
-
+}
+  
 export default Home;
